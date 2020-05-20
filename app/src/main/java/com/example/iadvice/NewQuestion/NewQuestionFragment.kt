a83d3d94ec4105b0ce15eceb65c2736e2788e831
Thomas <thomas.tasca95@gmail.com>
2020-05-20 15:36:56 +0200

    LiveData and Listeners moved

--- app/src/main/java/com/example/iadvice/NewQuestion/NewQuestionFragment.kt
+++ app/src/main/java/com/example/iadvice/NewQuestion/NewQuestionFragment.kt
@@ -5,22 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
-import android.widget.Switch
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.iadvice.R
 import com.example.iadvice.databinding.NewQuestionFragmentBinding
-import com.hbb20.CountryCodePicker
-import com.hbb20.CountryCodePicker.OnCountryChangeListener
 
 
 class NewQuestionFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private lateinit var binding: NewQuestionFragmentBinding
     private lateinit var viewModel: NewQuestionViewModel
-    var countryCodePicker: CountryCodePicker? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,14 +39,33 @@ class NewQuestionFragment : Fragment(), AdapterView.OnItemSelectedListener {
         // viewModelProviders used to not destroy the viewmodel until detached
         viewModel = ViewModelProvider(this, viewModelFactory).get(NewQuestionViewModel::class.java)
 
-        //bind views
-        countryCodePicker = binding.countrySpinner
-        binding.countrySpinner.visibility = View.GONE
-        //Setting Listeners for views
-        setPoll()
-        setCountrySwitch()
-        onSelectedCountry()
+
+        //Setting Listeners
         binding.createButton.setOnClickListener { onCreateNewQuestion() }
+        binding.countrySpinner.setOnCountryChangeListener { onSelectedCountry() }
+
+        binding.countrySwitch.setOnCheckedChangeListener {  _, isChecked ->
+            onShowCountry() }
+
+        /* Setting up LiveData observation relationship */
+        viewModel.visibility.observe(viewLifecycleOwner, Observer { isChecked ->
+            if (isChecked) {
+                binding.countrySpinner.visibility = View.VISIBLE
+            } else {
+                binding.countrySpinner.visibility = View.GONE
+            }
+        })
+
+        binding.pollSwitch.setOnCheckedChangeListener { _, isChecked ->
+            onShowPoll() }
+
+        viewModel.poll.observe(viewLifecycleOwner, Observer { isChecked ->
+            if (isChecked) {
+                Toast.makeText(activity,"Poll feature on", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(activity,"Poll feature off", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         return binding.root
     }
@@ -60,60 +76,36 @@ class NewQuestionFragment : Fragment(), AdapterView.OnItemSelectedListener {
         viewModel.durationSpinner = binding.durationSpinner
         viewModel.genderSpinner = binding.genderSpinner
         viewModel.categorySpinner = binding.categorySpinner
-
         viewModel.fillSPinners()
 
-        //Set all the ItemListener on the spinners
+        //TODO implement the interface on the ViewModel and move there the Spinners listener
         viewModel.durationSpinner.onItemSelectedListener = this
         viewModel.genderSpinner.onItemSelectedListener = this
         viewModel.categorySpinner.onItemSelectedListener = this
     }
 
-    private fun onCreateNewQuestion() {
-        viewModel.title = binding.argumentText.text.toString()
-        //TODO make the call for the DB creation
+
+    private fun onShowCountry() {
+        viewModel.onShowCountry( binding.countrySwitch.isChecked)
+    }
+
+    private fun onShowPoll() {
+        viewModel.onShowPoll( binding.pollSwitch.isChecked)
+    }
+
+    private fun onSelectedCountry(){
+        viewModel.onSelectedCountry(binding.countrySpinner!!.getSelectedCountryName().toString())
+        Toast.makeText( context, "Updated " + binding.countrySpinner!!.getSelectedCountryName(), Toast.LENGTH_SHORT).show()
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         val selected: String = parent?.getItemAtPosition(position).toString()
-        when(parent?.id){
-            R.id.gender_spinner -> viewModel.category = selected
-            R.id.duration_spinner -> viewModel.target = selected
-            R.id.gender_spinner -> viewModel.duration = selected
-        }
-    }
-
-    private fun setPoll(){
-        val switch: Switch = binding.pollSwitch
-        switch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                viewModel.poll = true
-                Toast.makeText(activity,"Poll feature on", Toast.LENGTH_SHORT).show()
-            } else {
-                viewModel.poll = false
-                Toast.makeText(activity,"Poll feature off", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-    }
-
-    private fun setCountrySwitch() {
-        val switch: Switch = binding.countrySwitch
-        switch.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding.countrySpinner.visibility = View.VISIBLE
-            } else {
-                binding.countrySpinner.visibility = View.GONE
-            }
-        }
-    }
-
-    private fun onSelectedCountry(){
-        countryCodePicker!!.setOnCountryChangeListener(OnCountryChangeListener {
-            Toast.makeText( context, "Updated " + countryCodePicker!!.getSelectedCountryName(), Toast.LENGTH_SHORT).show()
-            viewModel.selectedCountry = countryCodePicker!!.getSelectedCountryName().toString()
-        })
+        viewModel.onItemSelected(parent,selected)
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+    private fun onCreateNewQuestion() {
+        viewModel.onCreateNewQuestion()
+    }
 }
