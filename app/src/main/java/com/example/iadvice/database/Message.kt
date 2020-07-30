d7c3c0c0ac47afdf14f82be6e902f829532670e0
Roncax <paolo.roncaglioni@mail.polimi.it>
2020-07-30 12:53:10 +0200

    chat image + shared preferences

--- app/src/main/java/com/example/iadvice/database/Message.kt
+++ app/src/main/java/com/example/iadvice/database/Message.kt
@@ -5,7 +5,8 @@ import com.google.firebase.database.IgnoreExtraProperties
 @IgnoreExtraProperties
 data class Message(
     var chatId: String = "-1",
-    var user: String = "", //user a cui appartiene il messaggio
+    var user: String = "", //user_id message owner
+    var nickname: String = "", //nickname user
     var messageId: String = "",
     var text: String = "",
     var time: Long = 0
