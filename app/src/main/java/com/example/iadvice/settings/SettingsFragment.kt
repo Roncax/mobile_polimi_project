293f5a796abf1216116800830d946d1b8adc94a0
Thomas <thomas.tasca95@gmail.com>
2020-12-10 01:27:06 +0000

    load current text preferences

--- app/src/main/java/com/example/iadvice/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/iadvice/settings/SettingsFragment.kt
@@ -1,12 +1,45 @@
 package com.example.iadvice.settings
 
 import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
+import com.example.iadvice.PersistenceUtils
 import com.example.iadvice.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-       // setPreferencesFromResource(R.xml.preferences, rootKey)
         setPreferencesFromResource(R.xml.settings_fragment, rootKey)
+
+        displayCurrentValues()
+
+
+
+
+
+    }
+
+    private fun displayCurrentValues(){
+
+        //Display text values
+        val preferences = arrayOf("Name", "Nickname", "Age")
+        for (p in preferences)
+            displayCurrentText(p)
+
+        //Display list value (radio buttons)
+        val gender = findPreference<ListPreference>("Gender")
+       /// gender!!.summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
+
+
+    }
+
+    private fun displayCurrentText(preference: String) {
+        val namePref = findPreference<EditTextPreference>(preference)
+        when (preference) {
+            "Name" -> namePref!!.text = PersistenceUtils.currentUser.username
+            "Nickname" -> namePref!!.text = PersistenceUtils.currentUser.username
+            "Age" -> namePref!!.text = PersistenceUtils.currentUser.age.toString()
+        }
     }
 }
