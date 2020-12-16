fcf1aacf88e41bfc83a4710fbde9e6e0f1ed8faa
Thomas <thomas.tasca95@gmail.com>
2020-12-16 15:40:50 +0000

    try

--- app/src/main/java/com/example/iadvice/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/iadvice/settings/SettingsFragment.kt
@@ -1,45 +1,285 @@
 package com.example.iadvice.settings
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.preference.EditTextPreference
-import androidx.preference.ListPreference
-import androidx.preference.PreferenceFragmentCompat
-import com.example.iadvice.PersistenceUtils
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.iadvice.GlideApp
 import com.example.iadvice.R
-
-class SettingsFragment : PreferenceFragmentCompat() {
-
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.settings_fragment, rootKey)
-
-        displayCurrentValues()
-
-
+import com.example.iadvice.databinding.SettingsFragmentBinding
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.hbb20.CountryCodePicker.OnCountryChangeListener
+import kotlinx.android.synthetic.main.register_fragment.*
+import kotlinx.android.synthetic.main.register_fragment.add_image_register_button
+import kotlinx.android.synthetic.main.settings_fragment.*
 
 
+private const val TAG = "SettingsFragment"
 
-    }
+class SettingsFragment : Fragment() {
 
-    private fun displayCurrentValues(){
+    companion object {
 
-        //Display text values
-        val preferences = arrayOf("Name", "Nickname", "Age")
-        for (p in preferences)
-            displayCurrentText(p)
-
-        //Display list value (radio buttons)
-        val gender = findPreference<ListPreference>("Gender")
-       /// gender!!.summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
-
+        //image URI
+        var imageUri: Uri =
+            Uri.parse("gs://iadvice-49847.appspot.com/avatar_images/default_picture.png")
 
     }
 
-    private fun displayCurrentText(preference: String) {
-        val namePref = findPreference<EditTextPreference>(preference)
-        when (preference) {
-            "Name" -> namePref!!.text = PersistenceUtils.currentUser.username
-            "Nickname" -> namePref!!.text = PersistenceUtils.currentUser.username
-            "Age" -> namePref!!.text = PersistenceUtils.currentUser.age.toString()
+    private lateinit var viewModel: SettingsViewModel
+    private lateinit var binding: SettingsFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.settings_fragment, container, false
+        )
+
+        // viewModelProviders used to not destroy the viewmodel until detached
+        requireNotNull(this.activity).application
+
+        viewModel.getUser()
+
+/*
+        binding.apply {
+            registerButton.setOnClickListener {
+                if (binding.nicknameText.text.toString().isNotEmpty() and
+                    binding.ageRegisterText.text.toString().isNotEmpty() and
+                    binding.genderSpinner.selectedItem.toString().isNotEmpty() and
+                    binding.emailRegisterText.text.toString().isNotEmpty() and
+                    binding.firstPwText.text.toString().isNotEmpty()
+                ) {
+                    performRegister(binding)
+                } else {
+                    Toast.makeText(
+                        context, "You forgot a field, please fill and retry",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+            }
+
+            addImageRegisterButton.setOnClickListener {
+                val gallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+                startActivityForResult(gallery, 100)
+            }
+
+            categoriesButton.setOnClickListener {
+                showCategoriesDialog()
+            }
+
+        }
+        */
+
+
+        viewModel.gender.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.genderText.text = viewModel.gender.value
+        })
+
+        viewModel.age.observe(viewLifecycleOwner, Observer { newAge ->
+            binding.ageText.text = viewModel.age.value.toString()
+        })
+
+        viewModel.username.observe(viewLifecycleOwner, Observer { newUsername ->
+            binding.usernameText.text = viewModel.username.value
+        })
+
+        viewModel.country.observe(viewLifecycleOwner, Observer { newCountry ->
+            binding.countryText.text = viewModel.country.value
+        })
+
+
+        //Todo email, password e immagine
+
+
+
+
+        binding.apply {
+            ageRegisterText.afterTextChanged { viewModel.setAge(it.toInt()) }
+            nicknameText.afterTextChanged { viewModel.setUsername(it) }
+
+
+           genderSpinner.setOnItemSelectedListener(object : OnItemSelectedListener {
+                override fun onItemSelected(
+                    parentView: AdapterView<*>?,
+                    selectedItemView: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    viewModel.setGender(genderSpinner.selectedItem.toString())
+                }
+
+                override fun onNothingSelected(parentView: AdapterView<*>?) {
+                    //no code here
+                }
+            })
+
+          countrySpinner.setOnCountryChangeListener(OnCountryChangeListener {
+                viewModel.setCountry(countrySpinner.selectedCountryName)
+            })
+
+            //Set listener for categories button
+                categoriesButton.setOnClickListener {
+                    showCategoriesDialog()
+                }
+        }
+
+
+        //uploadDefaultImage()
+        return binding.root
+    }
+
+    //function used to recycle code for the EditText listeners of different fields
+    fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+            override fun afterTextChanged(editable: Editable?) {
+                afterTextChanged.invoke(editable.toString())
+            }
+        })
+    }
+
+
+    // Method to show an alert dialog with multiple choice list items
+    private fun showCategoriesDialog() {
+        // Late initialize an alert dialog object
+        lateinit var dialog: AlertDialog
+
+        // Initialize an array of colors
+        //TODO upload from array resource
+        val arrayCat = resources.getStringArray(R.array.categories)
+
+        // Initialize a boolean array of checked items
+        val arrayChecked = booleanArrayOf(false, false, false, true)
+
+
+        // Initialize a new instance of alert dialog builder object
+        val builder = AlertDialog.Builder(this.requireContext())
+
+        // Set a title for alert dialog
+        builder.setTitle("Choose categories of interest")
+
+        // Define multiple choice items for alert dialog
+        builder.setMultiChoiceItems(arrayCat, arrayChecked) { _, which, isChecked ->
+            // Update the clicked item checked status
+            arrayChecked[which] = isChecked
+
+            // Get the clicked item
+            val categories = arrayCat[which]
+
+
+        }
+
+        // Set the positive/yes button click listener
+        builder.setPositiveButton("OK") { _, _ ->
+
+            viewModel.categories = mutableListOf()
+            for (i in arrayCat.indices) {
+                val checked = arrayChecked[i]
+                if (checked) {
+                    viewModel.categories.add(arrayCat[i])
+                }
+            }
+        }
+
+
+        // Initialize the AlertDialog using builder object
+        dialog = builder.create()
+
+        // Finally, display the alert dialog
+        dialog.show()
+    }
+
+
+
+    /*
+    private fun performRegister(binding: SettingsFragmentBinding) {
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            binding.emailRegisterText.text.toString(),
+            binding.firstPwText.text.toString()
+        )
+            .addOnCompleteListener {
+                if (!it.isSuccessful) {
+                    Log.e(TAG, "Successfull registration")
+                }
+                val uid = it.result!!.user!!.uid
+                Log.d(TAG, "Successfull created user with uid: ${uid}")
+
+                viewModel.username = binding.nicknameText.text.toString()
+                viewModel.uid = uid
+                viewModel.age = binding.ageRegisterText.text.toString().toInt()
+                viewModel.gender = binding.genderSpinner.selectedItem.toString()
+                viewModel.uri = imageUri
+                viewModel.country = binding.countrySpinner.selectedCountryName
+
+                viewModel.registerUser()
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "Failed to create user: ${it.message}")
+            }
+
+        if (!requireView().findNavController().popBackStack()) {
+            Toast.makeText(
+                context, "Cannot go back to login, error!",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
+*/
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageUri = data?.data!!
+        if (resultCode == Activity.RESULT_OK && requestCode == 100) {
+            Glide.with(this@SettingsFragment)
+                .load(imageUri)
+                .fitCenter()
+                .circleCrop()
+                .into(add_image_register_button)
+        }
+    }
+
+    private fun uploadDefaultImage() {
+        val imagesRef: StorageReference? =
+            FirebaseStorage.getInstance().reference.child("avatar_images/default_picture.png")
+
+        binding.apply {
+            GlideApp.with(this@SettingsFragment)
+                .load(imagesRef)
+                .fitCenter()
+                .circleCrop()
+                .into(addImageRegisterButton)
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/iadvice/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/iadvice/settings/SettingsViewModel.kt
@@ -1,2 +1,129 @@
 package com.example.iadvice.settings
 
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.iadvice.database.User
+import com.example.iadvice.home.HomeFragmentViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+
+private const val TAG = "SettingsViewModel"
+
+class SettingsViewModel : ViewModel() {
+
+    lateinit var userId: String
+    lateinit var uri: Uri
+    lateinit var categories: MutableList<String>
+
+
+
+
+    private val _gender = MutableLiveData<String>()
+    val gender: LiveData<String>
+        get() = _gender
+
+    private val _country = MutableLiveData<String>()
+    val country: LiveData<String>
+        get() = _country
+
+    private val _username = MutableLiveData<String>()
+    val username: LiveData<String>
+        get() = _username
+
+    private val _age = MutableLiveData<Int>()
+    val age: LiveData<Int>
+        get() = _age
+
+
+    init{
+        _age.value = -1
+    }
+
+    fun setAge(age: Int){
+        _age.value = age
+       // updateUser()
+    }
+
+    fun setUsername(username: String){
+        _username.value = username
+        updateUser()
+    }
+
+    fun setGender(gender: String){
+        _gender.value = gender
+        updateUser()
+    }
+
+    fun setCountry(country: String){
+        _country.value = country
+        updateUser()
+    }
+
+
+     // update user with the selected parameters
+    fun updateUser() {
+        val db = Firebase.database.reference
+
+        val user = User(
+            uid = userId,
+            username = _username.value.toString(),
+            gender = _gender.value.toString(),
+            age = 5,
+            country = _country.value.toString()
+        )
+
+        db.child("users").child(userId).setValue(user)
+
+
+        for (category in categories) {
+            db.child("users").child(userId).child("categories").child(category).setValue("true")
+        }
+
+         /*
+        FirebaseStorage.getInstance().reference.child("avatar_images/$userId").putFile(uri)
+         */
+
+     }
+
+
+
+    fun getUser(){
+
+        userId = FirebaseAuth.getInstance().uid!!
+
+        val userListener = FirebaseDatabase.getInstance().reference
+            .child("users")
+            .child(userId)
+
+
+        userListener.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(dataSnapshot: DatabaseError) {
+                Log.e(TAG, "Cancelled ids retrieve on fetch user '$userId'")
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue<User>()
+                Log.d(TAG, "user '${user!!.uid}' retrieved")
+
+                _gender.value = user!!.gender
+                _username.value = user!!.username
+                _age.value = user!!.age.toInt()
+                _country.value = user!!.country
+                //TODO  gestione categories e uri e password!!!
+
+                }
+        })
+    }
+
+
+}
